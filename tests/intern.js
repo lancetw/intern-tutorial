ee4f5a61af28db9ba41da187088694c25c72a8f4
Hsin-lin Cheng <lancetw@gmail.com>
2015-04-17 16:43:14 +0800

    try unit test

--- tests/intern.js
+++ tests/intern.js
@@ -55,7 +55,7 @@ define({
 	suites: [ 'tests/hello' ],
 
 	// Functional test suite(s) to run in each browser once non-functional tests are completed
-	functionalSuites: [ 'tests/functional/index' ],
+	functionalSuites: [  ],
 
 	// A regular expression matching URLs to files that should not be included in code coverage analysis
 	excludeInstrumentation: /^(?:tests|node_modules)\//
